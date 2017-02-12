55ee82d451e2e292a9818ade02b509f4b98639e8
Brad Duthie <brad.duthie@gmail.com>
2017-02-12 15:38:12 +0000

    Create the function games_ga(), which returns a table of final strategies and their percentiles

--- genetic_algorithm.R
+++ genetic_algorithm.R
@@ -12,101 +12,85 @@ source("R/fitness.R");
 source("R/tournament.R");
 source("R/sample_round.R");
 
+games_ga <- function(CC = 2, CD = 0, DC = 3, DD = 1, callC = TRUE, 
+                     generations = 100, rounds = 20, num_opponents = 10){
 
-agents <- NULL;
-
-history_vec <- c(0,0,0,0,0,1,0,1,0,0,1,1,1,0,0,1,0,1,1,1,0,1,1,1);
-history     <- matrix(data = history_vec, ncol = 3, byrow = TRUE);
-hist_pretty <- history;
-hist_pretty[hist_pretty==0]   <- "C";
-hist_pretty[hist_pretty=="1"] <- "D";
-hist_print <- NULL
-for(i in 1:dim(hist_pretty)[1]){
-  row        <- paste(hist_pretty[i,], collapse=" ");
-  hist_print <- c(hist_print, row);
-}
-hist_print <- c(hist_print, "1st");
-
-for(i in 1:100){
-    agents[[i]] <- rbinom(n=9, size=1, prob=0.5);
-}
-
-payoffs <- c(2, 0, 3, 1);
-
-PD <- function(a1_play, a2_play){
-    points <- 0;
-    if(a1_play == 0 & a2_play == 0){
-        points <- payoffs[1];
+    agents      <- NULL;
+    history_vec <- c(0,0,0,0,0,1,0,1,0,0,1,1,1,0,0,1,0,1,1,1,0,1,1,1);
+    history     <- matrix(data = history_vec, ncol = 3, byrow = TRUE);
+    hist_pretty <- history;
+    
+    hist_pretty[hist_pretty==0]   <- "C";
+    hist_pretty[hist_pretty=="1"] <- "D";
+    hist_print                    <- NULL;
+    
+    for(i in 1:dim(hist_pretty)[1]){
+        row        <- paste(hist_pretty[i,], collapse=" ");
+        hist_print <- c(hist_print, row);
     }
-    if(a1_play == 0 & a2_play == 1){
-        points <- payoffs[2];
+    hist_print <- c(hist_print, "1st");
+
+    for(i in 1:100){
+        agents[[i]] <- rbinom(n=9, size=1, prob=0.5);
     }
-    if(a1_play == 1 & a2_play == 0){
-        points <- payoffs[3];
+
+    payoffs <- c(CC, CD, DC, DD);
+
+    AGENTS       <- NULL;
+    FITNESS      <- NULL;
+    mean_fitness <- NULL;
+
+    gen <- 0;
+
+    while(gen < generations){
+    
+        agents  <- crossover(agents = agents, prob = 0.05);
+    
+        agents  <- mutation(agents = agents, prob = 0.05);
+    
+        fitness <- check_fitness(history = history, agents = agents, 
+                                 pay = payoffs, useC = callC, rounds = rounds,
+                                 num_opponents = num_opponents);
+    
+        agents  <- tournament(agents = agents, fitness = fitness);
+    
+        gen <- gen + 1;
+    
+        AGENTS[[gen]]  <- agents;
+        FITNESS[[gen]] <- fitness;
     }
-    if(a1_play == 1 & a2_play == 1){
-        points <- payoffs[4];
+
+    MEAN_FITNESS <- lapply(X=FITNESS, FUN=mean);
+    MEAN_FITNESS <- unlist(MEAN_FITNESS);
+    
+    final_agent_vec <- unlist(agents);
+    final_agents    <- matrix(data=final_agent_vec, ncol = 9 , byrow = TRUE);
+
+    final_agents[final_agents==0]   <- "C";
+    final_agents[final_agents=="1"] <- "D";
+    strats <- NULL;
+    for(i in 1:100){
+        strats[[i]] <- paste(final_agents[i,], collapse=" ");
     }
-    return(points);
+
+    strats_tab <- table(strats);
+    strats_fr  <- as.numeric(strats_tab);
+    strats_or  <- order(strats_fr, decreasing=TRUE);
+    strats_fr  <- strats_fr[strats_or];
+    genomes    <- rownames(strats_tab[strats_or]);
+    uni_genome <- length(genomes);
+
+    tops <- matrix(data=0, nrow=uni_genome, ncol = 9);
+    for(i in 1:uni_genome){
+        split_geno <- strsplit(x=genomes[i], split = " ");
+        tops[i,]  <- split_geno[[1]];
+    }
+    summ_geno   <- rbind(hist_print, tops);
+    percentiles <- c("Final %", strats_fr[1:uni_genome]);
+    summ_geno   <- cbind(summ_geno, percentiles);
+    rownames(summ_geno) <- NULL
+    colnames(summ_geno) <- summ_geno[1,];
+    summ_geno <- summ_geno[-1,];
+    
+    return(summ_geno);
 }
-
-AGENTS       <- NULL;
-FITNESS      <- NULL;
-mean_fitness <- NULL;
-generations  <- 100;
-
-gen <- 0;
-
-while(gen < generations){
-    
-    agents  <- crossover(agents = agents, prob = 0.05);
-    
-    agents  <- mutation(agents = agents, prob = 0.05);
-    
-    fitness <- check_fitness(history = history, agents = agents, pay = payoffs,
-                             useC = TRUE);
-    
-    agents  <- tournament(agents = agents, fitness = fitness);
-    
-    gen <- gen + 1;
-    
-    AGENTS[[gen]]  <- agents;
-    FITNESS[[gen]] <- fitness;
-
-}
-
-
-final_agent_vec <- unlist(agents);
-final_agents    <- matrix(data=final_agent_vec, ncol = 9 , byrow = TRUE);
-
-
-MEAN_FITNESS <- lapply(X=FITNESS, FUN=mean);
-MEAN_FITNESS <- unlist(MEAN_FITNESS);
-
-final_agents[final_agents==0]   <- "C";
-final_agents[final_agents=="1"] <- "D";
-strats <- NULL;
-for(i in 1:100){
-    strats[[i]] <- paste(final_agents[i,], collapse=" ");
-}
-
-strats_tab <- table(strats);
-strats_fr  <- as.numeric(strats_tab);
-strats_or  <- order(strats_fr, decreasing=TRUE);
-strats_fr  <- strats_fr[strats_or];
-genomes    <- rownames(strats_tab[strats_or]);
-
-top_5 <- matrix(data=0, nrow=5, ncol = 9);
-for(i in 1:5){
-  split_geno <- strsplit(x=genomes[i], split = " ");
-  top_5[i,]  <- split_geno[[1]];
-}
-summ_geno   <- rbind(hist_print, top_5);
-percentiles <- c("Final %", strats_fr[1:5]);
-summ_geno   <- cbind(summ_geno, percentiles);
-rownames(summ_geno) <- NULL
-colnames(summ_geno) <- summ_geno[1,];
-summ_geno <- summ_geno[-1,];
-
-print(summ_geno);
-
